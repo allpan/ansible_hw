53606a20dd0c9589840b76815dc59d8fa75af315
pan <pan@pan.com>
2019-02-19 15:08:43 +0000

    work version infrastructure but app not work with db

--- roles/web/files/pan/my/webpage1/views.py
+++ roles/web/files/pan/my/webpage1/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 import platform
 import datetime
+from django.db import connection
+cursor = connection.cursor()
 
 
 # Create your views here.
 
 def webpage1(request):
-    date = "123"
+    rows = 0
     srv_name = platform.platform()
-    on_off = 'offline'
     date_time = datetime.datetime.now()
-
+    for row in cursor.execute("SELECT * FROM ptable"):
+        rows=row
+        
+    if rows == 0:
+        on_off = 'offline'
+    else: on_off = 'online'
+    
     return render(request, "./webpage1.html", locals())
