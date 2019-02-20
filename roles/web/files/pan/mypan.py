990f8f77cb1fd72c663022c32fa3e5b173e22dc3
Olexandr <forgit@bigmir.net>
2019-02-21 01:02:14 +0200

    Work version

--- roles/web/files/pan/mypan.py
+++ roles/web/files/pan/mypan.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import platform
+import datetime
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -19,13 +20,13 @@ def mypan():
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute("SELECT name FROM ptable")
-    row = ' <p style="color:forestgreen;">' + ''.join(cursor.fetchone()) + "</p>"
-	
+    row = '<p> connect to db pantest: ' + ''.join(cursor.fetchone()) + '</p>'
+#	date_time = '<p> ' + datetime.datetime.now() + '</p>'
     conn.commit()  
     conn.close()
   except Exception:
-    row = '<p style="color:red;"> Error connect to db.</p>'
-  return "<!DOCTYPE html><b>Hello World</b> from " + platform.platform() + row
+    row = '<p> Error connect to db pantest.</p>'
+  return "<!DOCTYPE html><b>Nginx+Gunicorn+Flask on server: </b>" + platform.platform() + row 
 
 if __name__ == "__main__":
   app.run()
